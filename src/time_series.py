a84403a800e9e5ec5b2a15c1f02c426fa033e8ce
Duke Dinh <dinh.duke@gmail.com>
2023-04-07 13:23:47 -0500

    update time_series.py

--- src/time_series.py
+++ src/time_series.py
@@ -10,37 +10,51 @@ mu = returns.mean().values
 cov = returns.cov().values
 
 # Define the financial time series prediction problem as a QUBO dictionary
-N = len(mu) # number of assets
-T = 20 # number of time steps
-q = 4 # number of quantum bits used to represent each asset
-h = np.zeros((T, N*q))
-J = np.zeros((T, N*q, N*q))
+N = len(mu)  # number of assets
+T = 20  # number of time steps
+q = 4  # number of quantum bits used to represent each asset
+h = np.zeros((T, N * q))
+J = np.zeros((T, N * q, N * q))
 for t in range(1, T):
     for i in range(N):
-        h[t,i*q:(i+1)*q] = -2*mu[i]*t/T
+        h[t, i * q:(i + 1) * q] = -2 * mu[i] * t / T
         for j in range(i, N):
-            J[t,i*q:(i+1)*q,j*q:(j+1)*q] = cov[i,j]*t/T
+            J[t, i * q:(i + 1) * q, j * q:(j + 1) * q] = cov[i, j] * t / T
 
 # Set up the D-Wave sampler and an embedding composite for mapping the problem to the D-Wave hardware
-sampler = DWaveSampler()
+sampler = DWaveSampler(solver={'qpu': True}, token='YOUR-OWN-DWAVE-API-TOKEN-HERE')
 embedding = EmbeddingComposite(sampler)
 
-# Submit the QUBO to the D-Wave quantum computer and retrieve the lowest energy solution
-response = sampler.sample_qubo(Q, num_reads=1000)
+# Define a QUBO problem
+q = {('x', 'x'): -1, ('y', 'y'): -1, ('x', 'y'): 2}
+
+# Submit the problem to the D-Wave solver and collect the results
+response = embedding.sample_qubo(q, num_reads=1000)
+
 samples = response.record.sample
 energies = response.record.energy
 
 # Process the results to obtain the predicted asset prices
+num_bits = 4
 predicted_prices = np.zeros((T, N))
 for t in range(1, T):
     for i in range(N):
-        bits = samples[0][t-1,i*q:(i+1)*q]
-        predicted_prices[t,i] = np.dot(np.arange(q), bits)/q*(data.iloc[t-1,i]+1)
+        bits = []
+        for j in range(len(samples)):
+            for k in range(num_bits):
+                bits.append(samples[j][t - 1, i * q + k])
+        predicted_prices[t, i] = np.dot(np.arange(q), bits) / q * (data.iloc[t - 1, i] + 1)
     for i in range(N):
-        for j in range(i+1, N):
-            bits_i = samples[0][t-1,i*q:(i+1)*q]
-            bits_j = samples[0][t-1,j*q:(j+1)*q]
-            predicted_prices[t,i] *= (1 + cov[i,j]/(q**2)*(np.dot(np.arange(q), bits_i)*np.dot(np.arange(q), bits_j)/q**2 - np.dot(bits_i, bits_j)))
+        for j in range(i + 1, N):
+            bits_i = []
+            bits_j = []
+            for k in range(len(samples)):
+                for l in range(q):
+                    bits_i.append(samples[k][t - 1, i * q + l])
+                    bits_j.append(samples[k][t - 1, j * q + l])
+            predicted_prices[t, i] *= (1 + cov[i, j] / (q ** 2) * (
+                        np.dot(np.arange(q), bits_i) * np.dot(np.arange(q), bits_j) / q ** 2 - np.dot(bits_i, bits_j)))
+
 
 # Print the predicted asset prices for the next 20 time steps
 print(predicted_prices)
